7a2cdf8b1ee6baf438a072f8fe4b9bbd9ad1a537
Aen <hello@aentan.com>
2019-06-03 15:22:10 +0800

    shuffle swarm

--- assets/js/swarm.js
+++ assets/js/swarm.js
@@ -197,6 +197,8 @@ var self = window;
       }
     }
 
+    nextArt = shuffle(nextArt);
+
     var seed = nextArt.length;
     // Recreate art particles, based on this seed
     createArtParticles(seed);
@@ -317,6 +319,21 @@ var self = window;
 
   function randomBetween(min, max) {
     return ~~(Math.random() * (max - min + 1) + min);
+  }
+
+  /*
+   * Shuffles array in place.
+   * @param {Array} a items An array containing the items.
+   */
+  function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
   }
 
 	/*
